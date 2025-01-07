0fef9b773ceaa1255c19fc8b3ccf795f4abc6f3f
22KChaturvedi <22KChaturvedi@S006-01A>
2025-01-07 11:42:47 +0000

    Add project files.

---
+++ introtovs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace introtovs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("world!");
+        }
+    }
+}
